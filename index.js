996ceb6856d975fc874f704df3ed0ed2428c094d
thiagonemecek <tgnemecek@gmail.com>
2020-05-31 23:32:26 -0400

    Add more skills to Toolbox

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
-let version = '1.2.2';
+const version = "1.2.3";
 
-window.onload = function() {
-    let versionEl = document.getElementById('version');
-    versionEl.innerHTML = "v" + version;
+window.onload = function () {
+  let versionEl = document.getElementById("version");
+  versionEl.innerHTML = "v" + version;
 
-    let options = {
-        strings: [
-            'Web',
-            'Full-Stack',
-            'Front-End',
-            'Game'
-        ],
-        typeSpeed: 80,
-        loop: true,
-        showCursor: false
-      };
-    
-    new Typed('#subtitle-typed', options);
-    AOS.init({
-        duration: 1000
-    });
-}
+  let options = {
+    strings: ["Web", "Full-Stack", "Front-End"],
+    typeSpeed: 80,
+    loop: true,
+    showCursor: false,
+  };
+
+  new Typed("#subtitle-typed", options);
+  AOS.init({
+    duration: 1000,
+  });
+};
